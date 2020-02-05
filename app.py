7aacf4f75af1e6e988aae4187aadd29ab7e88e92
Fumaz <itsfumaz@gmail.com>
2020-02-05 17:30:14 -0500

    finished website

--- app.py
+++ app.py
@@ -1,13 +1,75 @@
-from flask import Flask
+import json
+
+from flask import Flask, render_template
 
 import api
+from card import Status, Monitor, Subcard, Card
+
 app = Flask(__name__)
+
+
+def get_monitors():
+    result = json.loads(api.get_monitors())
+    monitors = {'status': result['stat'], 'cards': []}
+    for monitor in result['monitors']:
+        mon = Monitor(monitor['id'], monitor['friendly_name'], monitor['url'])
+        sub = Subcard(mon.name, mon, monitor['all_time_uptime_ratio'], get_status_name(monitor['status']),
+                      get_status_color(monitor['status']))
+        monitors['cards'].append(sub)
+    return monitors
+
+
+def get_status_name(status):
+    if status == 2 or status == 0:
+        return "Operational."
+    else:
+        return "Offline."
+
+
+def get_status_color(status):
+    if status == 2 or status == 0:
+        return "success"
+    else:
+        return "danger"
+
+
+def get_status(monitors):
+    if monitors['status'] == 'ok':
+        if (card.status_color == 'success' for card in monitors['cards']):
+            return Status('success', 'All systems are online and operational.')
+        else:
+            return Status('warning', 'Some systems are undergoing problems.')
+    else:
+        return Status('danger', 'All systems are undergoing problems.')
+
+
+def get_cards(monitors):
+    cards = [Card('System node'), Card('Health node')]
+    for subcard in monitors['cards']:
+        if subcard.name == 'Main server node':
+            cards[0].subcards.append(subcard)
+        else:
+            cards[1].subcards.append(subcard)
+
+    for card in cards:
+        success = (subcard.status == 'success' for subcard in card.subcards)
+        if success:
+            card.status = 'All systems are online and operational.'
+            card.status_color = 'success'
+        else:
+            card.status = 'Some systems are undergoing problems.'
+            card.status_color = 'warning'
+
+    return cards
 
 
 @app.route('/')
 def hello_world():
-    return api.get_monitors()
+    monitors = get_monitors()
+    cards = get_cards(monitors)
+    status = get_status(monitors)
+    return render_template('index.html', status=status, cards=cards)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0')
---
+++ card.py
@@ -0,0 +1,41 @@
+class Monitor:
+    def __init__(self, monitor_id, name, url):
+        self.monitor_id = monitor_id
+        self.name = name
+        self.url = url
+
+
+class Card:
+    def __init__(self, name: str, subcards: [] = None, status=None, status_color=None):
+        if subcards is None:
+            subcards = []
+
+        if status is None:
+            status = "System operational."
+
+        if status_color is None:
+            status_color = "success"
+
+        self.name = name
+        self.subcards = subcards
+        self.status = status
+        self.status_color = status_color
+        self.friendly_name = name.split(' ')[0]
+        self.status_c2 = '#7ED321' if status_color == 'success' else 'red'
+
+
+class Subcard:
+    def __init__(self, name: str, monitor: Monitor, uptime_ratio: str = "100.00", status="System operational.",
+                 status_color="success"):
+        self.name = name
+        self.status = status
+        self.status_color = status_color
+        self.monitor = monitor
+        self.uptime_ratio = uptime_ratio
+        self.status_c2 = '#7ED321' if status_color == 'success' else 'red'
+
+
+class Status:
+    def __init__(self, alert, text):
+        self.alert = alert
+        self.text = text
